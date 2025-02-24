8c867f507a7f12acc2b4085de4f0f07d1dac61ff
HlaingMinThan <hlaingminthan@92@gmail.com>
2025-02-24 15:55:18 +0700

    Ep-26-Ask-a-new-question-page

--- app/(root)/questions/components/QuestionForm.tsx
+++ app/(root)/questions/components/QuestionForm.tsx
@@ -2,18 +2,26 @@
 
 import React from "react";
 
+import Button from "@/components/Button";
 import Editor from "@/components/Editor";
+import Input from "@/components/Input";
 
 function QuestionForm() {
   const [value, setValue] = React.useState("");
   return (
-    <>
-      <Editor
-        label="Any Question ?"
-        value={value}
-        onChange={(v) => setValue(v)}
-      />
-    </>
+    <div className="space-y-5">
+      <h1 className="text-2xl font-bold">Ask A New Question?</h1>
+      <Input label="Title" text="Describe your question title in short way." />
+      <div className="mt-3">
+        <Editor
+          label="Any Question ?"
+          value={value}
+          onChange={(v) => setValue(v)}
+        />
+      </div>
+      <Input label="Tags" text="Please press enter to add a new tag." />
+      <Button>Create</Button>
+    </div>
   );
 }
 
